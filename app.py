fd7a8b4683a007a0a035dbc249ae5485b031d409
Gabriela Lira Bertolo <gabriela.bertolo@MacBook-Pro-de-CONTA.local>
2026-03-02 22:38:07 -0300

    commit 020326

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+import os
+import requests
+from flask import Flask, request
 
 app = Flask (__name__)
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 
 @app.route("/")
 def index():
   return "Olá, <b>tudo bem?</b>"
+
+@app.route("/teste")
+def teste():
+  return "Essa página é um <b>teste</b>! Beijos"
+
+@app.route("/telegram", methods=["POST"])
+def telegram_update():
+  update = request.json
+  url_envio_mensagem = f"hrrps://api.telegram.org/bot{BOT_TOKEN}/sendMessage"chat_id = update["message"]["chat"]["id"]
+  mensagem = {"chat_id": chat_id; "text": "mensagem <b>recebida</b>!", "parse_mode":"HTML"}
+requests.post(url_envio_mensagem, data=mensagem)
+return "ok"
