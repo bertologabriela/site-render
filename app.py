a72b3820ae1f6d4c9d218f01a47983a1d63e5b69
Gabriela Lira Bertolo <gabriela.bertolo@MacBook-Pro-de-CONTA.local>
2026-03-02 22:54:41 -0300

    fix: corrige sintaxe no app

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ def teste():
 @app.route("/telegram", methods=["POST"])
 def telegram_update():
   update = request.json
-  url_envio_mensagem = f"hrrps://api.telegram.org/bot{BOT_TOKEN}/sendMessage"chat_id = update["message"]["chat"]["id"]
+  url_envio_mensagem = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+chat_id = update["message"]["chat"]["id"]
   mensagem = {"chat_id": chat_id; "text": "mensagem <b>recebida</b>!", "parse_mode":"HTML"}
 requests.post(url_envio_mensagem, data=mensagem)
 return "ok"
