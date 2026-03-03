38548f4d0214e33ce4b09d7f14e5eeba83336e93
Gabriela Lira Bertolo <gabriela.bertolo@MacBook-Pro-de-CONTA.local>
2026-03-02 23:07:42 -0300

    fix final indentacao

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ def teste():
 def telegram_update():
   update = request.json
   url_envio_mensagem = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-chat_id = update["message"]["chat"]["id"]
-mensagem = {
+  chat_id = update["message"]["chat"]["id"]
+  mensagem = {
     "chat_id": chat_id,
     "text": "mensagem <b>recebida</b>!",
     "parse_mode": "HTML"
-}
-requests.post(url_envio_mensagem, data=mensagem)
-return "ok"
+    }
+  requests.post(url_envio_mensagem, data=mensagem)
+  return "ok"
